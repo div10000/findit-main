b2ce051833e8b3f0688228dd6f46079f161dacdd
DP-Ubuntu-Sentry <ironman1001ic@gmail.com>
2025-08-08 14:24:23 +0530

    keys hidden

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,15 +1,14 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore';
 import { getAuth } from "firebase/auth";
 
-
 const firebaseConfig = {
-    apiKey: "AIzaSyDQnEbfeWM0MZyVLKUrRuAb0HEZBSUoQLo",
-    authDomain: "findit-main.firebaseapp.com",
-    projectId: "findit-main",
-    storageBucket: "findit-main",
-    messagingSenderId: "446374013487",
-    appId: "1:446374013487:web:ea475a05845349b3d6b4da",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Ensure Firebase is initialized only once
